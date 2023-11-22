c7a5c5085c8d01a5c67d70a5b6d8a81afa77635a
Pedro Luis <pedrolssilva@hotmail.com>
2023-11-22 19:40:45 -0300

    Working with Theory and InlineData annotation in tests

--- alura.estacionamento/Alura.Estacionamento.Testes/PatioTestes.cs
+++ alura.estacionamento/Alura.Estacionamento.Testes/PatioTestes.cs
@@ -11,7 +11,7 @@ namespace Alura.Estacionamento.Testes
             // Arrange
             var estacionamento = new Patio();
             var veiculo = new Veiculo();
-            veiculo.Proprietario = "André Silva/";
+            veiculo.Proprietario = "André Silva";
             veiculo.Tipo = TipoVeiculo.Automovel;
             veiculo.Cor = "Verde";
             veiculo.Modelo = "Fusca";
@@ -23,6 +23,32 @@ namespace Alura.Estacionamento.Testes
 
             // Assert
             double faturamento = estacionamento.TotalFaturado();
+            Assert.Equal(2, faturamento);
+        }
+
+        [Theory]
+        [InlineData("André Silva", "ASD-1498", "preto", "Gol")]
+        [InlineData("José Silva", "POL-9242", "Cinza", "Fusca")]
+        [InlineData("Maria Silva", "GDR-6524", "Azul", "Opala")]
+        [InlineData("Pedro Silva", "GDR-0101", "Azul", "Corsa")]
+        public void ValidaFaturamentoComVariosVeiculos(string proprietario,
+            string placa, string cor, string modelo)
+        {
+            // Arrange
+            var estacionamento = new Patio();
+            var veiculo = new Veiculo();
+            veiculo.Proprietario = proprietario;
+            veiculo.Tipo = TipoVeiculo.Automovel;
+            veiculo.Cor = cor;
+            veiculo.Modelo = modelo;
+            veiculo.Placa = placa;
+            estacionamento.RegistrarEntradaVeiculo(veiculo);
+            estacionamento.RegistrarSaidaVeiculo(veiculo.Placa);
+
+            // Act
+            double faturamento = estacionamento.TotalFaturado();
+
+            // Assert
             Assert.Equal(2, faturamento);
         }
     }
